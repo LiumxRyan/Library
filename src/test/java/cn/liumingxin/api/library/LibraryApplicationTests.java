0ad252f70a075b2e3ce6d4baba7741963a4ac9d7
LiuMingxin <1258854493@qq.com>
2022-05-19 17:49:35 +0800

    项目框架切换为 SpringBoot 2.6.7

---
+++ src/test/java/cn/liumingxin/api/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liumingxin.api.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
